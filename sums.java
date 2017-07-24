2bf1bef49224a69190639eadb9d6b01294236639
Aksharakeerthi <akshara2223@gmail.com>
2017-07-24 14:03:47 +0530

    Create sums.java

---
+++ sums.java
@@ -0,0 +1,14 @@
+# sums
+import java.io.*
+import java.util.*;
+public class sums{
+int a=0,b=0;
+for(int =1; i<=15; i++){
+a+=i;
+}
+System.out.println(a);
+for(int j=15;j<=45j+=2){
+b+=j;
+}
+System.out.println(b);
+}
